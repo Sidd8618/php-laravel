ee89e8a66bc52bb33fe1472af6d85945027202e7
Kumar_Siddharth <161428277+Sidd8618@users.noreply.github.com>
2026-07-06 21:47:07 +0530

    Add files via upload

---
+++ helpers2.php
@@ -0,0 +1,5 @@
+<?php
+
+// NOTE: This file was referenced in composer.json's autoload.files section
+// but was missing from the provided project zip. Created as an empty
+// placeholder so Composer's autoloader can load without fatal errors.
